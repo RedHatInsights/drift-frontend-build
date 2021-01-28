12f85c3a91c41c98f85326e866095a56a8102fcd
Travis CI User <travis@example.org>
2021-01-28 22:23:17 +0000

    Update webpack config
    The appname used for CSS scoping was accidentaly removed in shared
    config @beta22.
    https://github.com/RedHatInsights/frontend-components/commit/9f7096bba002813ddd4de613a043e1eb52d50983#diff-0ff265c46140a3ca34889b853d585da568e7feee3420f9396a833bbd2584b556L54

---
+++ js/609.e3e730a1a1e7c8b4b6f4.js
@@ -0,0 +1,2 @@
+(self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[609],{41609:(e,r,t)=>{var n=t(280),f=t(64160),i=t(35694),o=t(1469),u=t(98612),p=t(44144),c=t(25726),l=t(36719),a=Object.prototype.hasOwnProperty;e.exports=function(e){if(null==e)return!0;if(u(e)&&(o(e)||"string"==typeof e||"function"==typeof e.splice||p(e)||l(e)||i(e)))return!e.length;var r=f(e);if("[object Map]"==r||"[object Set]"==r)return!e.size;if(c(e))return!n(e).length;for(var t in e)if(a.call(e,t))return!1;return!0}}}]);
+//# sourceMappingURL=609.e3e730a1a1e7c8b4b6f4.js.map
