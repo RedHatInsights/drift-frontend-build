c3cae292ed761196e284f85f82b6bc71e904474e
Travis CI User <travis@example.org>
2021-05-19 20:35:09 +0000

    [DRFT-275] Handle no hsp read permissions
    This PR blocks the HSP icon on:
    Add system modal
    Comparison table
    Create baseline modal
    
    To test:
    
    ```
        const handlePermissionsUpdate = (hasCompareRead, hasBaselinesRead, hasBaselinesWrite/*, hasHSPRead*/, hasInventoryRead) => {
            setPermissions({
                hasCompareReadPermissions: hasCompareRead,
                hasBaselinesReadPermissions: hasBaselinesRead,
                hasBaselinesWritePermissions: hasBaselinesWrite,
                hasHSPReadPermissions: /*hasHSPRead*/false,
                hasInventoryReadPermissions: hasInventoryRead,
                arePermissionsLoaded: true
            });
        };
    ```
    For each of the following, there should be no historical profiles column:
    Go to inventory/systems table on add system modal
    Drift -> Comparison -> click 'Add to comparison' button -> make sure 'Systems' tab is selected
    Go to inventory/systems table on create baseline modal
    Drift -> Baselines -> click 'Create baseline' button -> click 'Copy an existing system'
    If you create a comparison that includes a system with hsps or an hsp by itself, there should be no hsp icon in the header of the table under those items.

--- js/419.c16e89dcc9874124c15c.js
+++ js/419.c16e89dcc9874124c15c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[419],{14665:(t,e,n)=>{"use strict";function r(t,e){return(r=Object.setPrototypeOf||function(t,e){return t.__proto__=e,t})(t,e)}n.d(e,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/419.7ef92f769bc276163a0f.js.map
+//# sourceMappingURL=../sourcemaps/419.b490e382947fc18cf57e.js.map
--- js/655.7852c68b419ba54d1731.js
+++ js/655.7852c68b419ba54d1731.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[655],{70655:(e,t,r)=>{"use strict";function n(e,t){var r={};for(var n in e)Object.prototype.hasOwnProperty.call(e,n)&&t.indexOf(n)<0&&(r[n]=e[n]);if(null!=e&&"function"==typeof Object.getOwnPropertySymbols){var o=0;for(n=Object.getOwnPropertySymbols(e);o<n.length;o++)t.indexOf(n[o])<0&&Object.prototype.propertyIsEnumerable.call(e,n[o])&&(r[n[o]]=e[n[o]])}return r}r.d(t,{_T:()=>n})}}]);
-//# sourceMappingURL=../sourcemaps/655.7ef92f769bc276163a0f.js.map
+//# sourceMappingURL=../sourcemaps/655.b490e382947fc18cf57e.js.map
