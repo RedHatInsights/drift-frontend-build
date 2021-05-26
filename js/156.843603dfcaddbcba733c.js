0542b3d603e73cf34862cee3ddcd94aec3ca33de
Travis CI User <travis@example.org>
2021-05-26 19:56:14 +0000

    [DRFT-364] Fixes url clear on side nav Comparison click
    To repro:
    
    Select 1 or more systems for comparison
    Select Comparison from the left panel. The comparison table is still displayed. However, the url params are cleared.
    
    This PR correctly sets the url with the params.

--- js/156.843603dfcaddbcba733c.js
+++ js/156.843603dfcaddbcba733c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[156],{96156:(e,n,r)=>{"use strict";function t(e,n,r){return n in e?Object.defineProperty(e,n,{value:r,enumerable:!0,configurable:!0,writable:!0}):e[n]=r,e}r.d(n,{Z:()=>t})}}]);
-//# sourceMappingURL=../sourcemaps/156.b51df65f4f76bd097c98.js.map
+//# sourceMappingURL=../sourcemaps/156.dd66b7818ee013dfda33.js.map
--- js/419.c16e89dcc9874124c15c.js
+++ js/419.c16e89dcc9874124c15c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[419],{14665:(t,e,n)=>{"use strict";function r(t,e){return(r=Object.setPrototypeOf||function(t,e){return t.__proto__=e,t})(t,e)}n.d(e,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/419.b51df65f4f76bd097c98.js.map
+//# sourceMappingURL=../sourcemaps/419.dd66b7818ee013dfda33.js.map
