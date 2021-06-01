824d5b6cfbb58f19309aad52334527bd8ade31c0
Travis CI User <travis@example.org>
2021-06-01 19:02:09 +0000

    [DRFT-325] Disable 'Add system' button with no baselinesWrite perms
    To test:
    Go to line 29 in `src/App.js` and change to:
    ```
        const handlePermissionsUpdate = (hasCompareRead, hasBaselinesRead/*, hasBaselinesWrite*/, hasInventoryRead) => {
            setPermissions({
                hasCompareReadPermissions: hasCompareRead,
                hasBaselinesReadPermissions: hasBaselinesRead,
                hasBaselinesWritePermissions: /*hasBaselinesWrite*/false,
                hasInventoryReadPermissions: hasInventoryRead,
                arePermissionsLoaded: true
            });
        };
    ```
    Go to details for a baseline
    Select `Systems` tab
    
    'Add system' button should be disabled. If you revert the above code then you'll see that the button is now enabled.

--- js/156.843603dfcaddbcba733c.js
+++ js/156.843603dfcaddbcba733c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[156],{96156:(e,n,r)=>{"use strict";function t(e,n,r){return n in e?Object.defineProperty(e,n,{value:r,enumerable:!0,configurable:!0,writable:!0}):e[n]=r,e}r.d(n,{Z:()=>t})}}]);
-//# sourceMappingURL=../sourcemaps/156.aa838ac53a9af42aee8c.js.map
+//# sourceMappingURL=../sourcemaps/156.9931269c2c35b5565e64.js.map
--- js/419.c16e89dcc9874124c15c.js
+++ js/419.c16e89dcc9874124c15c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[419],{14665:(t,e,n)=>{"use strict";function r(t,e){return(r=Object.setPrototypeOf||function(t,e){return t.__proto__=e,t})(t,e)}n.d(e,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/419.aa838ac53a9af42aee8c.js.map
+//# sourceMappingURL=../sourcemaps/419.9931269c2c35b5565e64.js.map
