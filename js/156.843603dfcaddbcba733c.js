6ce95daf14a80f4a35c32f4a06dea59cc7b31443
Travis CI User <travis@example.org>
2021-06-04 17:42:01 +0000

    [DRFT-362] Deselect system from basket on different inventory page
    To repro:
    navigate to Add to comparison
    check a system on page 1
    change to next page (page 2)
    check a system
    open systems basket, uncheck all systems in it, click X button on the popover
    
    The system from page 1 is still in the basket.
    The Submit button is disabled
    Navigating to page 1, the system is not selected (although it's still in the basket)

--- js/156.843603dfcaddbcba733c.js
+++ js/156.843603dfcaddbcba733c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[156],{96156:(e,n,r)=>{"use strict";function t(e,n,r){return n in e?Object.defineProperty(e,n,{value:r,enumerable:!0,configurable:!0,writable:!0}):e[n]=r,e}r.d(n,{Z:()=>t})}}]);
-//# sourceMappingURL=../sourcemaps/156.a5070d828da9a6bf379b.js.map
+//# sourceMappingURL=../sourcemaps/156.c9fd5955e942d15182f2.js.map
--- js/419.c16e89dcc9874124c15c.js
+++ js/419.c16e89dcc9874124c15c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[419],{14665:(t,e,n)=>{"use strict";function r(t,e){return(r=Object.setPrototypeOf||function(t,e){return t.__proto__=e,t})(t,e)}n.d(e,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/419.a5070d828da9a6bf379b.js.map
+//# sourceMappingURL=../sourcemaps/419.c9fd5955e942d15182f2.js.map
