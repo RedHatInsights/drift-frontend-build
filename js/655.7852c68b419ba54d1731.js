691ad1883683004491a5d580f29c7823e9b669b6
Travis CI User <travis@example.org>
2021-06-04 17:40:42 +0000

    [DRFT-362] Deselect system from basket on different inventory page
    To repro:
    navigate to Add to comparison
    check a system on page 1
    change to next page (page 2)
    check a system
    open systems basket, uncheck all systems in it, click X button on the popover
    
    The system from page 1 is still in the basket.
    The Submit button is disabled
    Navigating to page 1, the system is not selected (although it's still in the basket)

--- js/655.7852c68b419ba54d1731.js
+++ js/655.7852c68b419ba54d1731.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[655],{70655:(e,t,r)=>{"use strict";function n(e,t){var r={};for(var n in e)Object.prototype.hasOwnProperty.call(e,n)&&t.indexOf(n)<0&&(r[n]=e[n]);if(null!=e&&"function"==typeof Object.getOwnPropertySymbols){var o=0;for(n=Object.getOwnPropertySymbols(e);o<n.length;o++)t.indexOf(n[o])<0&&Object.prototype.propertyIsEnumerable.call(e,n[o])&&(r[n[o]]=e[n[o]])}return r}r.d(t,{_T:()=>n})}}]);
-//# sourceMappingURL=../sourcemaps/655.9931269c2c35b5565e64.js.map
+//# sourceMappingURL=../sourcemaps/655.fbcb92af3d23305085aa.js.map
--- js/906.dded10df5ee97ed0d9a2.js
+++ js/906.dded10df5ee97ed0d9a2.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[906],{22122:(r,t,n)=>{"use strict";function e(){return(e=Object.assign||function(r){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var e in n)Object.prototype.hasOwnProperty.call(n,e)&&(r[e]=n[e])}return r}).apply(this,arguments)}n.d(t,{Z:()=>e})},19756:(r,t,n)=>{"use strict";function e(r,t){if(null==r)return{};var n,e,f={},u=Object.keys(r);for(e=0;e<u.length;e++)n=u[e],t.indexOf(n)>=0||(f[n]=r[n]);return f}n.d(t,{Z:()=>e})}}]);
-//# sourceMappingURL=../sourcemaps/906.9931269c2c35b5565e64.js.map
+//# sourceMappingURL=../sourcemaps/906.fbcb92af3d23305085aa.js.map
