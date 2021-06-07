92a935d2147b0929eac2de3ec040789e0b6e490f
Travis CI User <travis@example.org>
2021-06-07 12:19:31 +0000

    [DRFT 374] Fix selected basket sizing and scroll bar
    Persistent scroll bar in Selected Basket is gone and the minWidth and maxHeight have been increased.
    
    To test:
    Go to Comparison
    Click "Add system" button
    Add varying numbers of systems and click the `Selected (x)` in the top right of the modal
    
    You should see the basket adjust.

--- js/156.843603dfcaddbcba733c.js
+++ js/156.843603dfcaddbcba733c.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[156],{96156:(e,n,r)=>{"use strict";function t(e,n,r){return n in e?Object.defineProperty(e,n,{value:r,enumerable:!0,configurable:!0,writable:!0}):e[n]=r,e}r.d(n,{Z:()=>t})}}]);
-//# sourceMappingURL=../sourcemaps/156.fbcb92af3d23305085aa.js.map
+//# sourceMappingURL=../sourcemaps/156.6acf51f582c1d022232d.js.map
