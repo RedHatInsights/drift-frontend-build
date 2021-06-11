fa74415fff31d419b4f79939728881546f29d4d9
Travis CI User <travis@example.org>
2021-06-11 15:35:26 +0000

    Update packages to clear Dependabot CVE alerts

--- js/412.b0ed53b2d428fb488467.js
+++ js/412.b0ed53b2d428fb488467.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[412],{28412:(e,t,n)=>{"use strict";n.r(t),n.d(t,{default:()=>d});var r=n(25558),a=n.n(r),o=n(334),c=n(45237),l=n(69269),s=n(43946),u=n(83653);const d=function(){return a().createElement(c.Provider,{store:(0,l.S1)().getStore()},a().createElement(o.BrowserRouter,{basename:(0,u.Z)(window.location.pathname)},a().createElement(s.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.7768b6b6d830277a0c9a.js.map
+//# sourceMappingURL=../sourcemaps/412.82be894750362bb66f7a.js.map
--- js/419.c16e89dcc9874124c15c.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[419],{14665:(t,e,n)=>{"use strict";function r(t,e){return(r=Object.setPrototypeOf||function(t,e){return t.__proto__=e,t})(t,e)}n.d(e,{Z:()=>r})}}]);
-//# sourceMappingURL=../sourcemaps/419.7768b6b6d830277a0c9a.js.map
