15d8eabec3dc5371b09d53649657dfa8f4be38d5
Travis CI User <travis@example.org>
2021-08-12 17:31:29 +0000

    [DRFT-316] Pass columns to SystemsTable
    Before, we changed the value of `columns` in the `InventoryTable` via logic in the `InventoryTable` reducer. Now, we pass the columns in through the parent components and it allows the sizing to remain static.
    
    Also fixed issue with `Last seen` column resizing on sort click in Notifications table. To reproduce:
    
    open a baseline, navigate to system notification tab
    open Add system table
    'Last seen' header cell is resized when sorting by Last seen. The text is ommitted ("Last...") unless sorting by ascending.
    
    Now, the 'Last seen' column, and all other columns for that matter, stay the same size.

--- js/412.751a73b6ac28a1e7b9c5.js
+++ js/412.751a73b6ac28a1e7b9c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[412],{28412:(e,t,n)=>{"use strict";n.r(t),n.d(t,{default:()=>d});var r=n(25558),a=n.n(r),o=n(334),c=n(45237),l=n(69269),s=n(43946),u=n(83653);const d=function(){return a().createElement(c.Provider,{store:(0,l.S1)().getStore()},a().createElement(o.BrowserRouter,{basename:(0,u.Z)(window.location.pathname)},a().createElement(s.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.587017643fa644953cf8.js.map
+//# sourceMappingURL=../sourcemaps/412.39316ebf65777d6f8402.js.map
