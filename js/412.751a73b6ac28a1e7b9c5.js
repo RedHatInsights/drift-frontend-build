e7c5d4364f8f2ed0720770d4e5506b2fdc49f8f6
Travis CI User <travis@example.org>
2021-08-17 14:01:32 +0000

    [DRFT-563] Align icons in system header to system name
    To repro:
    Create a comparison
    
    The icons next to the names of the systems/baselines/hsps in the table header are aligned with the bottom of the names. This PR aligns them in the center.

--- js/412.751a73b6ac28a1e7b9c5.js
+++ js/412.751a73b6ac28a1e7b9c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[412],{28412:(e,t,n)=>{"use strict";n.r(t),n.d(t,{default:()=>d});var r=n(25558),a=n.n(r),o=n(334),c=n(45237),l=n(69269),s=n(43946),u=n(83653);const d=function(){return a().createElement(c.Provider,{store:(0,l.S1)().getStore()},a().createElement(o.BrowserRouter,{basename:(0,u.Z)(window.location.pathname)},a().createElement(s.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.25cd2afd3ee4703e4107.js.map
+//# sourceMappingURL=../sourcemaps/412.e4b571bfe05e1f2ad19e.js.map
