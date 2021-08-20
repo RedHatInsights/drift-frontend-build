44901a62d63319a06d56b78e3e57f21a6e566af4
Travis CI User <travis@example.org>
2021-08-20 13:41:07 +0000

    [DRFT-559] Align HSP icon and badge in inventory table
    To repro:
    Open inventory table
    Find a system with hsps and select at least 1 hsp from the popover
    
    The badge and hsp icon are slightly misaligned. This PR properly aligns them.

--- js/491.34fcacddebfd3a39bbab.js
+++ js/491.34fcacddebfd3a39bbab.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[491],{36491:(e,t,n)=>{"use strict";n.r(t);var r=n(25558),o=n.n(r),a=n(32169),l=n.n(a),c=n(334),u=n(45237),d=n(69269),s=n(43946),m=n(83653);const f=function(){return o().createElement(u.Provider,{store:(0,d.S1)().getStore()},o().createElement(c.BrowserRouter,{basename:(0,m.Z)(window.location.pathname)},o().createElement(s.Z,null)))};l().render(o().createElement(f,null),document.getElementById("root"))}}]);
-//# sourceMappingURL=../sourcemaps/491.1538454859f8cad7d26e.js.map
+//# sourceMappingURL=../sourcemaps/491.e3a2e972cd3f7dbb8c66.js.map
