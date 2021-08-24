6d1348e191b6de98ab31944091be2a1e2126ec4b
Travis CI User <travis@example.org>
2021-08-24 14:27:57 +0000

    Adds USE_CLOUD=true to insights-proxy commands in README (#503)

--- js/412.751a73b6ac28a1e7b9c5.js
+++ js/412.751a73b6ac28a1e7b9c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[412],{28412:(e,t,n)=>{"use strict";n.r(t),n.d(t,{default:()=>d});var r=n(25558),a=n.n(r),o=n(334),c=n(45237),l=n(69269),s=n(43946),u=n(83653);const d=function(){return a().createElement(c.Provider,{store:(0,l.S1)().getStore()},a().createElement(o.BrowserRouter,{basename:(0,u.Z)(window.location.pathname)},a().createElement(s.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.e3a2e972cd3f7dbb8c66.js.map
+//# sourceMappingURL=../sourcemaps/412.c89dd64ab270a69ef4c1.js.map
