6bd84722d9ffb32614ea4ec7bbe327c24d1a134c
Travis CI User <travis@example.org>
2021-08-26 15:23:04 +0000

    [DRFT-608] Fix case-sensitivity in active fact filters
    To repro:
    If my filter has all lower case, it will match as I type it through hitting return.  If my filter has any upper case, it will match as I type, but no longer after I hit return.
    
    Before, the app didn't properly handle case-insensitivity with active fact filters (pressing enter after a filter is added). Now, if a fact is added to active fact filters, it properly handles it.

--- js/412.751a73b6ac28a1e7b9c5.js
+++ js/412.751a73b6ac28a1e7b9c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[412],{28412:(e,t,n)=>{"use strict";n.r(t),n.d(t,{default:()=>d});var r=n(25558),a=n.n(r),o=n(334),c=n(45237),l=n(69269),s=n(43946),u=n(83653);const d=function(){return a().createElement(c.Provider,{store:(0,l.S1)().getStore()},a().createElement(o.BrowserRouter,{basename:(0,u.Z)(window.location.pathname)},a().createElement(s.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.7d5940681d4959b10168.js.map
+//# sourceMappingURL=../sourcemaps/412.ae0f5e73f4e29ac3fa3f.js.map
