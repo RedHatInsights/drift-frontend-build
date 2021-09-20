9356082e17f2521c632df44cd86c98587c9d9ec9
Travis CI User <travis@example.org>
2021-09-20 22:54:20 +0000

    Update frontend components
    This PR updates:
    frontend-components
    frontend-components-utilities
    frontend-components-notifications
    frontend-components-config

--- js/412.751a73b6ac28a1e7b9c5.js
+++ js/412.751a73b6ac28a1e7b9c5.js
@@ -1,2 +1,2 @@
 (self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[412],{28412:(e,t,n)=>{"use strict";n.r(t),n.d(t,{default:()=>d});var r=n(25558),a=n.n(r),o=n(334),c=n(45237),l=n(69269),s=n(43946),u=n(83653);const d=function(){return a().createElement(c.Provider,{store:(0,l.S1)().getStore()},a().createElement(o.BrowserRouter,{basename:(0,u.Z)(window.location.pathname)},a().createElement(s.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/412.7f8640ef3066a1ea35fc.js.map
+//# sourceMappingURL=../sourcemaps/412.5c84ad21954eb9fbe544ff85f25135a5.js.map
