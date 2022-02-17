1c096bbaff68fa3460c91a6b93c5a4ebbcbb278c
Travis CI User <travis@example.org>
2022-02-17 08:22:39 +0000

    Review and fix development setup

--- js/684.1645024571967.3b760e2d97aad900f86f.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[684],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>d});var r=n(92950),a=n.n(r),o=n(86235),c=n(37282),l=n(99655),s=n(43946),u=n(83653);const d=function(){return a().createElement(c.Provider,{store:(0,l.S1)().getStore()},a().createElement(o.BrowserRouter,{basename:(0,u.Z)(window.location.pathname)},a().createElement(s.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/684.89404ff308d61aee2444f6b0030f333b.js.map
