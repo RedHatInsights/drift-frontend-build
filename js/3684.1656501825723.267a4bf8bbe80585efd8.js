9c47c405d1c8feeb839da8bcab8bd2eb2a752c9b
Travis CI User <travis@example.org>
2022-07-19 15:17:52 +0000

    [DRFT-1148] Update FEC version
    This update allows Baselines text filter to accept letters. The filter should work normally.

--- js/3684.1656501825723.267a4bf8bbe80585efd8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdrift_frontend=self.webpackChunkdrift_frontend||[]).push([[3684],{28412:(e,t,n)=>{n.r(t),n.d(t,{default:()=>d});var r=n(92950),a=n.n(r),o=n(86235),c=n(37282),l=n(99655),s=n(43946),u=n(83653);const d=function(){return a().createElement(c.Provider,{store:(0,l.S1)().getStore()},a().createElement(o.BrowserRouter,{basename:(0,u.Z)(window.location.pathname)},a().createElement(s.Z,null)))}}}]);
-//# sourceMappingURL=../sourcemaps/3684.ea3bdf3eb7b7f99958e39f808884ead6.js.map
